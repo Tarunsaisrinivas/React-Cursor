a5b3b37854b29bac95f8b55ecdefccebb78cd082
Tarun Sai Srinivas <tarunsaisrinivas4@gmail.com>
2025-07-04 19:06:24 +0530

    added github url

--- app/containers/Navbar.jsx
+++ app/containers/Navbar.jsx
@@ -115,7 +115,7 @@ export default function Navbar() {
 
             {/* GitHub icon */}
             <a
-              href="#"
+              href="https://github.com/Tarunsaisrinivas/React-Cursor"
               target="_blank"
               rel="noopener noreferrer"
               className="p-2 rounded-full backdrop-blur-md hover:bg-gray-200/40 dark:hover:bg-white/10 transition"
